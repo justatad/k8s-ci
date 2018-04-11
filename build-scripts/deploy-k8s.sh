9e11a5cd39f314efa559adf0300b76ed4f368ea9
Donald Carnegie <donald.carnegie@slalom.com>
2018-04-11 15:36:24 +0100

    debugging:

--- build-scripts/deploy-k8s.sh
+++ build-scripts/deploy-k8s.sh
@@ -3,9 +3,6 @@ GREEN='\033[0;32m'
 NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
 echo -e "${GREEN}==== Deploying RBAC role ====${NC}"
-ls
-cd ..
-ls
 cd rbac
 for f in $(find ./ -name '*.yaml' -or -name '*.yml'); do kubectl apply -f $f --validate=false; done
 echo -e "${GREEN}==== Done deploying iam role ====${NC}"
