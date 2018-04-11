e536a10b0d80e5c534990a987b3bde17fc0cd1e7
Donald Carnegie <donald.carnegie@slalom.com>
2018-04-11 11:38:32 +0100

    Debugging Terraform

--- build-scripts/deploy-terraform.sh
+++ build-scripts/deploy-terraform.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
  
 #script to recursively travel a dir of n levels
+
  
 function traverse() {  
  
-for file in "ls $1"
+for file in `ls $1`
 do
     #current=${1}{$file}
     if [ ! -d ${1}${file} ] ; then
     echo ''
     else
  
-        echo -e "${GREEN}==== Deploying TF in ${1}${file} ====${NC}"
+        echo -e "${GREEN}==== Deploying TF in ${1}/${file} ====${NC}"
         cd ${1}${file}
  
         if ls | grep -q .tf;
         then
-            terraform init
-            terraform plan
-            terraform apply
+            #terraform init
+            #terraform plan
+            #terraform apply
+            echo -e "${GREEN}==== TF found to deploy ====${NC}"
         fi
  
-         echo -e "${GREEN}==== Done deploying TF in ${1}${file} ====${NC}"
+         echo -e "${GREEN}==== Done deploying TF in ${1}/${file} ====${NC}"
         ls
         echo "Traversing ${1}/${file} next"
         traverse "${1}/${file}"
@@ -35,7 +37,9 @@ NC='\033[0;0m'
 export PATH=$PATH:$(pwd)
  
 echo -e "${GREEN}==== Deploying terraform ====${NC}"
- 
+
+
+cd build-scripts
 traverse ../
  
 echo -e "${GREEN}==== Done deploying terraform  ====${NC}"
